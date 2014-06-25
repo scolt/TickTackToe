55b6afde9ee6db7897307c33bbae6af18b678ae7
Viktar Parashchanka <Viktar_Parashchanka@epam.com>
2014-06-25 14:42:16 +0300

    New project structure.

--- js/app/controller.js
+++ js/app/controller.js
@@ -1,7 +1,7 @@
 function ControllerClassic (config, service){
-	this.config=config;
-	this.service=service;
-	this.currentUser=1;
+	this.config = config;
+	this.service = service;
+	this.currentUser = 1;
 	var types;
 	this.clearField = function () {
 		var tictak = document.getElementsByClassName(this.config.path)[0];
@@ -21,16 +21,16 @@ function ControllerClassic (config, service){
 
 		/* Create matrix for game*/
 		input.appendChild(table);
-		var self=this;
+		var self = this;
 		for (var i = 0; i < this.config.sizeHeight; i++) {
 			for (var j = 0; j < this.config.sizeWidth; j++) {
 				var block = document.createElement('div');
 				var tmp = 'y' + i + 'x' + j;
 				block.className = 'position ' + tmp;
 				block.style.height = block.style.lineHeight = this.config.height / this.config.sizeHeight + 'px';
-				block.style.width=this.config.width / this.config.sizeWidth + 'px';
+				block.style.width = this.config.width / this.config.sizeWidth + 'px';
 				block.style.fontSize = this.config.width / this.config.sizeWidth / 2 + 'px';
-				block.addEventListener('click', function(e){
+				block.addEventListener('click', function(){
 					self.handler(this);
 				});
 				table.appendChild(block);
@@ -59,18 +59,18 @@ function ControllerClassic (config, service){
 		/*Check horizontal*/
 		if (fl) {
 			win.push([i, j]);
-			while (this.game.getPosition(i, j) == this.game.getPosition(i, k)) {
+			while (this.game.getPosition(i, j)  ==  this.game.getPosition(i, k)) {
 				win.push([i, k]);
 				count++;
 				k++;
 			}
 			k = +j - 1;
-			while (this.game.getPosition(i, j) == this.game.getPosition(i, k)) {
+			while (this.game.getPosition(i, j)  ==  this.game.getPosition(i, k)) {
 				win.push([i, k]);
 				count++;
 				k--;
 			}
-			if (count >= this.config.winCombination - 1)
+			if (count >=  this.config.winCombination - 1)
 				fl = false;
 			else
 				win = [];
@@ -80,18 +80,18 @@ function ControllerClassic (config, service){
 			k = +i + 1;
 			count = 0;
 			win.push([i, j]);
-			while (k <= this.config.sizeHeight - 1 && this.game.getPosition(i, j) == this.game.getPosition(k, j)) {
+			while (k <=  this.config.sizeHeight - 1 && this.game.getPosition(i, j)  ==  this.game.getPosition(k, j)) {
 				win.push([k, j]);
 				count++;
 				k++;
 			}
 			k = +i - 1;
-			while (k >= 0 && this.game.getPosition(i, j) == this.game.getPosition(k, j)) {
+			while (k >=  0 && this.game.getPosition(i, j)  ==  this.game.getPosition(k, j)) {
 				win.push([k, j]);
 				count++;
 				k--;
 			}
-			if (count >= this.config.winCombination - 1)
+			if (count >=  this.config.winCombination - 1)
 				fl = false;
 			else
 				win = [];
@@ -102,7 +102,7 @@ function ControllerClassic (config, service){
 			g = +j + 1;
 			count = 0;
 			win.push([i, j]);
-			while (k < this.config.sizeHeight && g <= this.config.sizeWidth && this.game.getPosition(i, j) == this.game.getPosition(k, g)) {
+			while (k < this.config.sizeHeight && g <=  this.config.sizeWidth && this.game.getPosition(i, j)  ==  this.game.getPosition(k, g)) {
 				win.push([k, g]);
 				count++;
 				k++;
@@ -110,13 +110,13 @@ function ControllerClassic (config, service){
 			}
 			k = +i - 1;
 			g = +j - 1;
-			while (k >= 0 && this.game.getPosition(i, j) == this.game.getPosition(k, g)) {
+			while (k >=  0 && this.game.getPosition(i, j)  ==  this.game.getPosition(k, g)) {
 				win.push([k, g]);
 				count++;
 				k--;
 				g--;
 			}
-			if (count >= this.config.winCombination-1)
+			if (count >=  this.config.winCombination-1)
 				fl = false;
 			else
 				win = [];
@@ -127,7 +127,7 @@ function ControllerClassic (config, service){
 			g = +j - 1;
 			count = 0;
 			win.push([i, j]);
-			while (k <= this.config.sizeHeight - 1 && g <= this.config.sizeWidth - 1 && this.game.getPosition(i, j) == this.game.getPosition(k, g)) {
+			while (k <=  this.config.sizeHeight - 1 && g <=  this.config.sizeWidth - 1 && this.game.getPosition(i, j)  ==  this.game.getPosition(k, g)) {
 				win.push([k, g]);
 				count++;
 				k++;
@@ -135,24 +135,20 @@ function ControllerClassic (config, service){
 			}
 			k = +i - 1;
 			g = +j + 1;
-			while (k >= 0 && this.game.getPosition(i, j) == this.game.getPosition(k, g)) {
+			while (k >=  0 && this.game.getPosition(i, j)  ==  this.game.getPosition(k, g)) {
 				win.push([k, g]);
 				count++;
 				k--;
 				g++;
 			}
-			if (count >= this.config.winCombination - 1)
-				fl = false;
-			else
-				win = [];
 		}
-		if (count >= this.config.winCombination - 1) {
+		if (count >=  this.config.winCombination - 1) {
 			this.win(win);
 		}
 	};
 	this.handler = function(obj) {
-		var y=obj.className.substr(10, 1);
-		var x=obj.className.substr(12, 1);
+		var y = obj.className.substr(10, 1);
+		var x = obj.className.substr(12, 1);
 		if(!this.game.getPosition(y,x)){
 			switch (this.currentUser) {
 				case 1:
@@ -163,11 +159,11 @@ function ControllerClassic (config, service){
 							break;
 						case 'images':
 							obj.style.background = 'url("'+types.items[0]+'") 50% 50%';
-							obj.style.backgroundSize="100% 100%";
+							obj.style.backgroundSize = "100% 100%";
 							break;
 					}
 					this.check(y,x);
-					this.currentUser=2;
+					this.currentUser = 2;
 					break;
 				case 2:
 					this.game.setPosition(this.currentUser, y, x);
@@ -177,25 +173,25 @@ function ControllerClassic (config, service){
 							break;
 						case 'images':
 							obj.style.background = 'url("'+types.items[1]+'") 50% 50%';
-							obj.style.backgroundSize="100% 100%";
+							obj.style.backgroundSize = "100% 100%";
 							break;
 					}
 					this.check(y,x);
-					this.currentUser=1;
+					this.currentUser = 1;
 					break;
 			}
 		}
 
 	};
 	this.win = function(arr){
-		for(var i=0; i<arr.length; i++){
-			var el=document.getElementsByClassName('y'+arr[i][0]+'x'+arr[i][1])[0];
-			el.style.backgroundColor="#dcffd1";
+		for(var i = 0; i<arr.length; i++){
+			var el = document.getElementsByClassName('y'+arr[i][0]+'x'+arr[i][1])[0];
+			el.style.backgroundColor = "#dcffd1";
 		}
-		var win=document.createElement('div');
-		win.className='winWindow';
-		win.innerHTML='Player ' + this.currentUser + ' WIN';
-		var input=document.getElementsByClassName('table')[0];
+		var win = document.createElement('div');
+		win.className = 'winWindow';
+		win.innerHTML = 'Player ' + this.currentUser + ' WIN';
+		var input = document.getElementsByClassName('table')[0];
 		input.appendChild(win);
 	};
 	this.init = function() {
@@ -205,42 +201,42 @@ function ControllerClassic (config, service){
 		types = this.game.getItems();
 	};
 	this.restart = function(){
-		this.currentUser=1;
+		this.currentUser = 1;
 		this.clearField();
 		this.init();
 	};
 }
 
-ControllerZombie.prototype=new ControllerClassic();
+ControllerZombie.prototype = new ControllerClassic('', '');
 function ControllerZombie (config, service){
-	this.config=config;
-	this.service=service;
+	this.config = config;
+	this.service = service;
 	this.check = function(i,j) {
-		var r=0, p= 0, q=0;
-		var count=0;
-		var win=[];
+		var r = 0, p =  0, q = 0;
+		var count = 0;
+		var win = [];
 
 		/* Check left */
-		for(var n=0; n<this.config.sizeWidth-1; n++){
-			for(var m=0; m<this.config.sizeHeight-1; m++){
-				count=0;
-				win=[];
+		for(var n = 0; n<this.config.sizeWidth-1; n++){
+			for(var m = 0; m<this.config.sizeHeight-1; m++){
+				count = 0;
+				win = [];
 				try{
-					if(this.game.stats[i][j]==this.game.stats[n][m]){
-						p=n;
-						q=m;
-						r=p+this.config.winCombination-1;
-						while(this.game.getPosition(i, j)==this.game.getPosition(p, q) && this.game.getPosition(i, j)==this.game.getPosition(r,q)){
-							if(r==p){
+					if(this.game.stats[i][j] == this.game.stats[n][m]){
+						p = n;
+						q = m;
+						r = p+this.config.winCombination-1;
+						while(this.game.getPosition(i, j) == this.game.getPosition(p, q) && this.game.getPosition(i, j) == this.game.getPosition(r,q)){
+							if(r == p){
 								win.push([r,q]);
-								if(count>=this.config.winCombination-1) {
+								if(count >= this.config.winCombination-1) {
 									this.win(win);
 									return true;
 								}
 							}
 							win.push([p,q]);
 							win.push([r,q]);
-							count=count+2;
+							count = count+2;
 							q++;
 							p++;
 							r--;
@@ -250,16 +246,16 @@ function ControllerZombie (config, service){
 				catch(e){
 					console.log(e);
 				}
-				count=0;
-				win=[];
+				count = 0;
+				win = [];
 				try{
-					if(this.game.getPosition(i,j)==this.game.getPosition(n, m)){
-						p=n;
-						q=m;
-						r=p;
+					if(this.game.getPosition(i,j) == this.game.getPosition(n, m)){
+						p = n;
+						q = m;
+						r = p;
 						win.push([i,j]);
-						while(r>0 && this.game.getPosition(i, j)==this.game.getPosition(p, q) && this.game.getPosition(i, j)==this.game.getPosition(r, q)){
-							if(r!=p){
+						while(r > 0 && this.game.getPosition(i, j) == this.game.getPosition(p, q) && this.game.getPosition(i, j) == this.game.getPosition(r, q)){
+							if(r != p){
 								count++;
 							}
 							count++;
@@ -268,7 +264,7 @@ function ControllerZombie (config, service){
 							q++;
 							p++;
 							r--;
-							if(count>=this.config.winCombination) {
+							if(count >= this.config.winCombination) {
 								this.win(win);
 								return true;
 							}
@@ -280,16 +276,17 @@ function ControllerZombie (config, service){
 				}
 			}
 		}
+		return false;
 	};
 	this.win = function(arr){
-		for(var i=0; i<arr.length; i++){
-			var el=document.getElementsByClassName('y'+arr[i][0]+'x'+arr[i][1])[0];
-			el.style.backgroundColor="#dcffd1";
+		for(var i = 0; i<arr.length; i++){
+			var el = document.getElementsByClassName('y'+arr[i][0]+'x'+arr[i][1])[0];
+			el.style.backgroundColor = "#dcffd1";
 		}
-		var win=document.createElement('div');
-		win.className='winWindow zombie';
-		win.innerHTML=this.currentUser + ' WIN!<br>ARGGRH!';
-		var input=document.getElementsByClassName('table')[0];
+		var win = document.createElement('div');
+		win.className = 'winWindow zombie';
+		win.innerHTML = this.currentUser + ' WIN!<br>ARGGRH!';
+		var input = document.getElementsByClassName('table')[0];
 		input.appendChild(win);
 	};
 }
--- js/js.js
+++ js/js.js
@@ -1,33 +1,24 @@
+var conf={};
+conf.sizeWidth=3;
+conf.sizeHeight=4;
+conf.width=300;
+conf.height=300;
+conf.winCombination=3;
+conf.reset=true;
+conf.path='TickTackToe';
+var mods=[{
+		Controller: ControllerClassic,
+		Model: TickTackToe,
+		name: 'Classic Mode'
+	},
+	{
+		Controller: ControllerZombie,
+		Model: TickTackToeZombie,
+		name: 'Zombie Mode'
+	}
+];
 addEventListener("DOMContentLoaded", function(){
-//	var a=document.getElementsByClassName('TickTackToe')[0];
-//	var config=new Array();
-//	config.push('<div class="config">');
-//	config.push('<label for="size">Size</label> <input type="number" name="size" id="size" value="3"/>');
-//	config.push('<label for="width">Width</label> <input type="number" name="width" id="width" value="420" step="10"/>');
-//	config.push('<label for="winCombination">Win Combination</label> <input type="number" name="winCombination" id="winCombination" value="3"/>');
-//	config.push('<a href="#" id="submitConfig">Create</a>')
-//	config.push('</div>');
-//
-//	config=config.join('');
-//	a.innerHTML=config;
-//	document.getElementById('submitConfig').addEventListener('click', function(){
-//		var conf={};
-//		conf.size=document.getElementById('size').value;
-//		conf.width=document.getElementById('width').value;
-//		conf.winCombination=document.getElementById('winCombination').value;
-//		conf.reset=true;
-//		conf.path='TickTackToe';
-//		var c =new controller(conf);
-//	})
-	var conf={};
-		conf.sizeWidth=3;
-		conf.sizeHeight=4;
-		conf.width=300;
-		conf.height=300;
-		conf.winCombination=3;
-		conf.reset=true;
-		conf.path='TickTackToe';
-	var c = new ControllerClassic(conf, TickTackToeZombie);
+	var i = 0, j = 0;
+	var c = new mods[i].Controller(conf, mods[j].Model);
 	c.init();
-
 });
